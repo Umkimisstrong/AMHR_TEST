b2f50f44ef8c4fc5cd98ff6d8802474feea5e80f
KIM SANG KI <96037218+Umkimisstrong@users.noreply.github.com>
2024-05-30 17:00:19 +0900

    [AMHR] UI ADDED

--- Web/AMHR_WEB/Controllers/BoardController.cs
+++ Web/AMHR_WEB/Controllers/BoardController.cs
@@ -9,9 +9,15 @@ namespace AMHR_WEB.Controllers
 {
     public class BoardController : BaseController
     {
+        /// <summary>
+        /// CONTROLLER_NAME : 전역변수 Controller Name
+        /// </summary>
+        public const string CONTROLLER_NAME = "Board";
         public ActionResult BoardList(BoardContract contract)
-        { 
-            
+        {
+            ViewBag.FIRST_BREADCRUMB_NAME = CONTROLLER_NAME;
+            ViewBag.SECOND_BREADCRUMB_NAME = "BoardList";
+
             return View();
         }
     }
