79f940fdcfda498a628ec1b71de4227577a4a30e
KIM SANG KI <96037218+Umkimisstrong@users.noreply.github.com>
2024-06-21 13:22:23 +0900

    [AMHR] FileUpload Library Added

--- Web/AMHR_WEB/App_Start/BundleConfig.cs
+++ Web/AMHR_WEB/App_Start/BundleConfig.cs
@@ -24,6 +24,11 @@ namespace AMHR_WEB
             bundles.Add(new ScriptBundle("~/bundles/jquery_ui").Include(
                             "~/Scripts/jquery-ui.min.js"
                         ));
+            bundles.Add(new ScriptBundle("~/bundles/jquery_fileupload").Include(
+                            "~/Content/js/demo.js",
+                            "~/Content/js/jquery.*",
+                            "~/Content/js/vendor/jquery.ui.widget.js"
+                        ));
 
             /* Bundle */
             bundles.Add(new Bundle("~/bundles/bootstrap").Include(
@@ -41,6 +46,9 @@ namespace AMHR_WEB
                       "~/Content/bootstrap.min.css",
                       "~/Content/site.css"
                       ));
+            bundles.Add(new StyleBundle("~/Content/css/jquery").Include(
+                "~/Content/css/jquery.*"
+                ));
             bundles.Add(new StyleBundle("~/bundles/amhrCss").Include(
                      "~/StaticContents/css/amhr_*"));
         }
---
+++ Web/AMHR_WEB/Controllers/AttachFilesController.cs
@@ -0,0 +1,42 @@
+﻿using AMHR_WEB.Models;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AMHR_WEB.Controllers
+{
+    public class AttachFilesController : BaseController
+    {
+        public JsonResult Upload(HttpPostedFileBase files)
+        {
+            var resultList = new List<UploadFilesResult>();
+
+            string path = Server.MapPath("~/Content/uploads");
+            files.SaveAs(path + files.FileName);
+
+            UploadFilesResult uploadfiles = new UploadFilesResult();
+            uploadfiles.name = files.FileName;
+            uploadfiles.size = files.ContentLength;
+            uploadfiles.type = "image/jpeg";
+            uploadfiles.url = "/Content/uploads/" + files.FileName;
+            uploadfiles.thumbnailUrl = "/Content/uploads" + files.FileName;
+            uploadfiles.deleteType = "GET";
+
+            resultList.Add(uploadfiles);
+
+            JsonFiles jFiles = new JsonFiles(resultList);
+
+            return Json(jFiles);
+            
+        }
+
+        public JsonResult Delete(string file)
+        {
+            System.IO.File.Delete(Path.Combine(Server.MapPath("~/Content/uploads/"), file));
+            return Json("OK", JsonRequestBehavior.AllowGet);
+        }
+    }
+}
